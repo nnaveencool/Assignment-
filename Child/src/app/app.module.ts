d17801bb73a49663f3ead5daa666652d363a82bb
nnaveencool <nnaveencool@gmail.com>
2024-06-12 15:00:56 +0530

    Update app.module.ts
    routemodule code added

--- Child/src/app/app.module.ts
+++ Child/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
